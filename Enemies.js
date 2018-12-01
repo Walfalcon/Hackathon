a54633bfb7291285961542c5ac05845b37d19a93
Walfalcon <hyperlucas@gmail.com>
2018-12-01 13:42:20 -0800

    Update Enemies.js

--- Enemies.js
+++ Enemies.js
@@ -14,10 +14,24 @@ var Enemy1 = {
 		enemy.body.gravity.y = this.gravity;
 		enemy.maxHealth = this.health;
 		enemy.health = this.health;
+		enemy.animations.add('move', [0,1], 4);
+		enemy.animations.play('move', null, true);
 	},
 	
 	update: function (stage) {
-		
+		game.physics.arcade.collide(this.enemies, Stage.walls);
+		this.enemies.forEachAlive(this.enemyUpdate, this, stage);
+	},
+	
+	enemyUpdate: function (enemy, stage) {
+		//find the tile directly in front of the enemy
+		var tilePos = Math.floor((enemy.scale.x == 1 ? enemy.x - 1 : enemy.x +16)/16) + Math.floor(enemy.y/16)*stage.width;
+		if(stage.stage[tilePos + stage.width] == 0)
+		{
+			enemy.anchor.x = enemy.scale.x == 1 ? 1 : 0;
+			enemy.scale.x = -enemy.scale.x;
+		}
+		enemy.body.velocity.x = -enemy.scale.x * this.speed;
 	}
 };
 	
