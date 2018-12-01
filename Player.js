6198c0f24e18538dfd63ebf46d2f4fad46669ff9
chetgurevitch <chetgurevitch@protonmail.com>
2018-12-01 13:18:59 -0800

    Fix player animations

--- Player.js
+++ Player.js
@@ -2,11 +2,9 @@ var Player = {
 	create: function(x, y) {
 		player = game.add.sprite(x, y, 'player');
 		game.physics.arcade.enable(player);
-		player.body.bounce.y = 0.2;
+		player.body.bounce.y = 0.000001;
 		player.body.gravity.y = 300;
 		player.body.collideWorldBounds = true;
-		player.animations.add('right', [0], 10, true);
-		player.animations.add('jump', [2], 10, true);
 	},
 
 	update: function() {
@@ -21,7 +19,7 @@ var Player = {
 			player.body.velocity.x = -150;
 		}
 
-		if (keys.z.isDown)
+		if (keys.z.isDown && player.body.velocity.y > -1 && player.body.velocity.y < 0)
 		{
 			player.body.velocity.y = -200;
 		}
@@ -32,10 +30,10 @@ var Player = {
 		if (player.body.velocity.x > 0) {
 			player.frame = 0;
 		}
-		if (player.body.velocity.x == 0 && player.body.velocity.y == 0) {
+		if (player.body.velocity.x == 0 && player.body.acceleration.y == 0) {
 			player.frame = 1;
 		}
-		if (player.body.velocity.y > 1 || player.body.velocity < 1) {
+		if (player.body.velocity.y < -0.834 || player.body.velocity.y > 0) {
 			player.frame = 2;
 		}
 	}
