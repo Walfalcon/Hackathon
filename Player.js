d667258c798efdf5c535048d03a43c6fde028909
chetgurevitch <chetgurevitch@protonmail.com>
2018-12-01 13:30:22 -0800

    Improve player movement

--- Player.js
+++ Player.js
@@ -3,7 +3,7 @@ var Player = {
 		player = game.add.sprite(x, y, 'player');
 		game.physics.arcade.enable(player);
 		player.body.bounce.y = 0.000001;
-		player.body.gravity.y = 300;
+		player.body.gravity.y = 1200;
 		player.body.collideWorldBounds = true;
 	},
 
@@ -19,21 +19,20 @@ var Player = {
 			player.body.velocity.x = -150;
 		}
 
-		if (keys.z.isDown && player.body.velocity.y > -1 && player.body.velocity.y < 0)
+		if (keys.z.isDown && player.body.touching.down)
 		{
-			player.body.velocity.y = -200;
+			player.body.velocity.y = -400;
 		}
 
 		if (player.body.velocity.x < 0) {
 			player.frame = 0;
-		}
-		if (player.body.velocity.x > 0) {
+		} else if (player.body.velocity.x > 0) {
 			player.frame = 0;
 		}
-		if (player.body.velocity.x == 0 && player.body.acceleration.y == 0) {
+		if (player.body.velocity.x == 0 && player.body.touching.down) {
 			player.frame = 1;
 		}
-		if (player.body.velocity.y < -0.834 || player.body.velocity.y > 0) {
+		if (!player.body.touching.down) {
 			player.frame = 2;
 		}
 	}
